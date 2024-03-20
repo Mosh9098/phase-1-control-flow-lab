6dcf5c9243d8a92930fb19387b5268de4b420651
mosh9098 <mosh9098@gmail.com>
2024-03-20 22:35:42 +0300

    complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ridefeet){
+
+  let result
+  if (ridefeet <= 400) {
+    result = "This one is on me!";
+  }
+  else if(ridefeet > 400 && ridefeet <= 2000) {
+    result = "That will be twenty bucks.";
+  }
+  else if(ridefeet >= 2000 ) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if(ridefeet > 2500) {
+    result = "No can do.";
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const City = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return City;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity('NYC')); 
+console.log(ternaryCheckCity('Pittsburgh'));   
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+      case 'generous':
+          return "Thank you so much.";
+      case 'not as generous':
+          return "Thank you.";
+      default:
+          return "Bye.";
+  }
 }
